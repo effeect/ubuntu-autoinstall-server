bb50a47915caebbb86c3bb7feefb46af12fbffd5
effeect <oliver.dimes3@gmail.com>
2026-02-25 15:07:21 +0900

    Initial Commit

---
+++ Readme.md
@@ -0,0 +1,9 @@
+# Ubuntu-Autoinstall
+
+This is a repo that contains a basic Ubuntu-Desktop Autoinstall config for Ubuntu Desktop use and an Express server to statically generate it.
+
+In order to use it, you need a valid .env file with a variable containing the password for the desktop config (which will get hashed)
+
+# How to use :
+
+Simply start the program and when in the Ubuntu autoinstall, link to the express server that is currently running like so : `http://your.ip.address.here:3000/desktop-config`
---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const dotenv = require("dotenv");
+const fs = require("fs");
+const yaml = require("js-yaml");
+const { spawnSync } = require("child_process");
+const PORT = 3000;
+
+dotenv.config();
+
+// Check if the password variable has been set
+if (!process.env.PASSWORD) {
+  console.error("No Password detected, check your environment file. Exiting");
+  process.exit(1);
+}
+
+app.get("/desktop-config", (req, res) => {
+  // Need to do some trickery and add the password from the .env
+  const filePath = path.join(__dirname, "public", "desktop-config");
+  const fileContents = fs.readFileSync(filePath, "utf8");
+  let doc = yaml.load(fileContents, "utf8");
+  // Doing password hashing with openssl
+  const result = spawnSync("openssl", ["passwd", "-6", process.env.PASSWORD]);
+  const hashedPassword = result.stdout.toString().trim();
+
+  doc.autoinstall = doc.autoinstall || {};
+  doc.autoinstall.identity = doc.autoinstall.identity || {};
+  doc.autoinstall.identity.password = hashedPassword;
+
+  const updated = yaml.dump(doc, {
+    lineWidth: -1,
+    quotingType: '"',
+    noCompatMode: true,
+  });
+  res.type("text/yaml");
+  res.send(updated);
+});
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`✅ Static server running on port ${PORT}`);
+  console.log(`Check user-data: http://localhost:${PORT}/user-data`);
+});
